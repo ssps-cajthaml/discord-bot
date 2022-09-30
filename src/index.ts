dd8927e99f586fc4968f0346263f0ce6d1944eb6
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2022-09-30 22:36:26 +0200

    feat: purge command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import bot from "./bot";
 import ping from "./command/commands/ping";
+import purge from "./command/commands/purge";
 
 bot({
     commands: [
-        ping
+        ping,
+        purge
     ]
 });
