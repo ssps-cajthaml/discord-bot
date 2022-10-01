c5f7a1ef1d0a1e25c4594306d45cd1fa708874e1
Denis <Lenger>
2022-10-01 14:53:27 +0200

    Updated index to feature SayCommand

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import bot from "./bot";
 import ping from "./command/commands/ping";
 import purge from "./command/commands/purge";
-import { voice } from "./module/modules/voice";
+import say from "./command/commands/say";
 
 bot({
     commands: [
         ping,
-        purge
-    ],
-    modules: [
-        voice
+        purge,
+        say
     ]
 });
