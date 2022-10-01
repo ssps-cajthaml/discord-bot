afb42a15a2d6eed1e15f46603192730e7367ff80
Denis <Lenger>
2022-10-01 14:55:59 +0200

    Added modules back

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import bot from "./bot";
 import ping from "./command/commands/ping";
 import purge from "./command/commands/purge";
 import say from "./command/commands/say";
+import { voice } from "./module/modules/voice";
 
 bot({
     commands: [
         ping,
         purge,
         say
+    ],
+    modules: [
+        voice
     ]
 });
